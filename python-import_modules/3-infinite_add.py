f665972b960d2ff3b78406c86b0410c043b90c1f
Luis <luisayala323@gmail.com>
2023-06-02 19:59:33 -0400

    fix

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    import sys
-    i = 0
-    k = 0
-    x = int(sys.argv[1:])
-    for i, arg in enumerate(x, start=1):
-        k += arg
+    argc = len(sys.argv) - 1
+
+    if argc == 0:
+        print(0)
+    else:
+        total = sum(int(arg) for arg in sys.argv[1:])
+        print(total)
