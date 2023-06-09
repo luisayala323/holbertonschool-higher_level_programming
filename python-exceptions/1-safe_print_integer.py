a2b2828bbd192ba5574fd48f36a92c5cd5659f30
Luis <luisayala323@gmail.com>
2023-06-09 19:59:41 -0400

    update

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         count = 0
         for item in my_list:
             if count < x:
-                print(item, end="")
+                print("{:d}".format(item), end="")
                 count += 1
         print()
         return count
