7f435f68e833f162cec71cefbb91dcf73e977dad
Luis <luisayala323@gmail.com>
2023-06-14 17:27:29 -0400

    fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
 
-
-# This module defines a Square class.
+The Square class represents a geometric square shape.
+"""
 
 
 class Square:
     """
     This class represents a square shape.
 
-
+    Attributes:
+        No attributes are defined for this class.
     """
     pass
