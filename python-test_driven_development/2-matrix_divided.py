ae9024f8b398bf2335d87514c3736df0b82b825b
Luis <luisayala323@gmail.com>
2023-06-15 20:58:40 -0400

    divide matrix

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module contains a function that divide a mtrix
+named matrix_divided that you pass 2 arguments
+the matrix and the number to divide for
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    This function divide a matrix by the
+    number number passed to us
+
+    matrix = matrix to be divided
+    div = number to be divided from
+    """
+    msg_error = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(matrix) is not list:
+        raise TypeError(msg_error)
+
+    if type(div) not in [int, float]:
+        raise TypeError('div must be a number')
+
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+
+    for row in matrix:
+        for value in row:
+            if type(value) not in [int, float]:
+                raise TypeError(msg_error)
+
+    first_row = len(matrix[0])
+    for row in matrix:
+        if first_row != len(row):
+            raise TypeError('Each row of the matrix must have the same size')
+
+    return [[round(i / div, 2) for i in row] for row in matrix]
