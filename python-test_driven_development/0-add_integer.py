05f11af530bd67698ac2a80f7ccee48679327d1a
Luis <luisayala323@gmail.com>
2023-06-15 21:27:31 -0400

    over indented fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -15,7 +15,7 @@ def add_integer(a, b=98):
         recreate add both.
     """
     if type(a) is not int and type(a) is not float:
-            raise TypeError('a must be an integer')
+        raise TypeError('a must be an integer')
 
     if type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
