d8d9ca15f406872a4e898e6b025e7e262b5c096d
Luis <luisayala323@gmail.com>
2023-06-15 21:01:02 -0400

    function that prints my name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module will have a function
+the prints the sentece
+(My name is [first_name] [last_name])
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+        Function that check that first_name is a str,
+        as well as last_name. If both are str it will
+        print the sentece
+        (My name is [first_name] [last_name])
+    """
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+
+    print("My name is {} {}".format(first_name, last_name))
