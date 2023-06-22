e8d62dd5420270c0b4b5ae1e134e98aa968c0ccb
Luis <luisayala323@gmail.com>
2023-06-22 10:28:34 -0400

    whitespace fix

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,8 +4,7 @@
 
 
 def is_same_class(obj, a_class):
-    """Check if he object is exactly 
-an instance of the specified class 
-otherwise return True or False"""
+    """Check if he object is exactly an instance
+      of the specified class otherwise return True or False"""
 
     return type(obj) is a_class
