f016c108073368553defadf6e57c35f7a6b0df1b
Luis <luisayala323@gmail.com>
2023-06-22 21:23:11 -0400

    checker fix issues

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+
 """Defines a text file-reading functionality"""
 
 
 def reading(filename=""):
-    """read and prints  UTF8 text file"""
+    """
+    read and prints  UTF8 text file
+    Args: filename (string): name of the file
+
+    """
+
     with open(filename, 'r') as file:
         print(file.read(), end="")
