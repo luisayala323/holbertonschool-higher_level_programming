19f71561b9c2d118bcdef96153f6137c7ab78e57
Luis <luisayala323@gmail.com>
2023-06-22 21:29:44 -0400

    fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,10 +8,10 @@ and prints it
 
 def reading(filename=""):
     """
-    read and prints  UTF8 text file
+    Function that reads and prints  UTF8 text file
     Args: filename (string): name of the file
 
     """
 
-    with open(filename, 'r') as file:
-        print(file.read(), end="")
+    with open(filename, 'r') as f:
+        print(f.read(), end="")
