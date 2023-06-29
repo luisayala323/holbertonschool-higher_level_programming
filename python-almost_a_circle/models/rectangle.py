857c9455403c2e1a7ce732364aade8fa7cbf6022
Luis <luisayala323@gmail.com>
2023-06-29 19:45:33 -0400

    obtain str rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -141,3 +141,12 @@ class Rectangle(Base):
         """
         for _ in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """
+        Returns the string representation of the Rectangle object.
+
+        Returns:
+            str: The string representation of the Rectangle.
+        """
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
