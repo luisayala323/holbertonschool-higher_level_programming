f7079c3778aeb1fd422b646c4a075dbf518106da
Luis <luisayala323@gmail.com>
2023-06-29 20:00:41 -0400

    display update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -139,8 +139,9 @@ class Rectangle(Base):
         """
         Prints the rectangle using the '#' character.
         """
+        print("\n" * self.__y, end="")
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """
