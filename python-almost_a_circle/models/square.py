a84fefa261e68b392421289696fae5d8d4e7ea18
Luis <luisayala323@gmail.com>
2023-06-29 21:02:13 -0400

    square update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -46,3 +46,21 @@ class Square(Rectangle):
         Print the string representation of the Square method.
         """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def update(self, *args, **kwargs):
+        """
+        Accept both positional arguments and keyword arguments.
+        """
+        num_args = len(args)
+        if num_args == 0:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+        else:
+            if num_args > 0:
+                self.id = args[0]
+            if num_args > 1:
+                self.size = args[1]
+            if num_args > 2:
+                self.x = args[2]
+            if num_args > 3:
+                self.y = args[3]
