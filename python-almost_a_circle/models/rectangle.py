6cfd4fdb3b5d14e02176a865cd0e1b9c5f1bc315
Luis <luisayala323@gmail.com>
2023-06-29 21:12:51 -0400

    rectangle dictionary

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -167,3 +167,13 @@ class Rectangle(Base):
         if kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns a dictionary representation of the Rectangle"""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
