cc83ce77d43e3388abac6732b191089a17f7417b
Luis <luisayala323@gmail.com>
2023-06-29 21:29:30 -0400

    dict to json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,16 +1,96 @@
 #!/usr/bin/python3
 
-"""" Module: class name Base"""
+
+"""
+This module contains the Base class.
+"""
+
+from os import path
+import json
 
 
 class Base:
-    """class name base"""
+    """
+    Base class
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """constructor method"""
+        """
+        Initialize Base instance
+
+        Args:
+            id (int, optional): id to assign to
+             the instance. Defaults to None.
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): list of dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns Python obj of JSON string representation.
+        """
+
+        if json_string is None or len(json_string) == 0:
+            json_string = "[]"
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of list_objs to a file.
+        """
+
+        filename = cls.__name__ + ".json"
+        list_dicts = []
+        if list_objs is not None:
+            for obj in list_objs:
+                list_dicts.append(cls.to_dictionary(obj))
+        with open(filename, 'w') as file:
+            file.write(cls.to_json_string(list_dicts))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Returns instance with attributes already set.
+        """
+
+        if cls.__name__ == "Square":
+            dummy = cls(1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Return a list of instance
+        """
+
+        filename = cls.__name__ + '.json'
+        if path.exists(filename) is False:
+            return []
+        with open(filename, 'r') as fd:
+            attrs_dic = cls.from_json_string(fd.read())
+            li = []
+            for i in attrs_dic:
+                li.append(cls.create(**i))
+            return li
