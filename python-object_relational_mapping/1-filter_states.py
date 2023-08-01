ef0b0ca0f72f51f68e7d2aeaf00d2d59c3b25c97
Luis <luisayala323@gmail.com>
2023-08-01 18:35:09 -0400

    lis states with starts with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+
+def list_states_with_n(username, password, database_name):
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database_name,
+            port=3306
+        )
+
+        # Create a cursor object to interact with the database
+        cursor = db.cursor()
+
+        # Execute the SQL query to retrieve all states with names starting with 'N'
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+
+        # Fetch all rows from the result set
+        rows = cursor.fetchall()
+
+        # Display the results
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        # If there is an error, print the error message
+        print("Error:", e)
+
+    finally:
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
+
+
+# Check if the script is run as the main module
+if __name__ == "__main__":
+    # Check if all three arguments are provided (username, password, database name)
+    if len(sys.argv) != 4:
+        # Print the usage message and exit with an error code of 1
+        print("Usage: python script_name.py <username> <password> <db_name>")
+        sys.exit(1)
+
+    # Get the arguments from the command line
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Call the function to list states with names starting with 'N' from the database
+    list_states_with_n(username, password, database_name)
